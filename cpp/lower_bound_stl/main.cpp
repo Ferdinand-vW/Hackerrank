8a5827f1b9b511618d71abedf8562becee0c2d83
Ferdinand van Walree <ferdinandvanwalree@gmail.com>
2020-07-09 22:09:33 +0100

    implemented lower bound stl

---
+++ cpp/lower_bound_stl/main.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    std::cin >> n;
+
+    std::vector<int> numbers;
+
+    while (n > 0) {
+      int i;
+      std::cin >> i;
+      numbers.push_back(i);
+      n--;
+    }
+
+    int q;
+    std::cin >> q;
+    while (q > 0) {
+      int i;
+      std::cin >> i;
+
+      auto it = std::lower_bound(numbers.begin(),numbers.end(),i);
+      
+      if (*it == i) {
+        std::cout << "Yes " << (it - numbers.begin()) << endl;
+      } else {
+        std::cout << "No " << (it - numbers.begin()) << endl;
+      }
+    q--;
+    }
+    
+    return 0;
+}
